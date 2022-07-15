5060ca51b99a2afde0a6d073b2d1e8d7149e4924
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-07-15 21:04:30 +0800

    turtle moves around the screen with player input

---
+++ snake2.py
@@ -0,0 +1,37 @@
+import turtle
+
+# Set up screen
+turtle.setup(650,650)
+wn = turtle.Screen()
+wn.bgcolor('navy')
+
+# Create player turtle
+player = turtle.Turtle()
+player.color('darkorange')
+player.shape('turtle')
+player.penup()
+
+player.penup()
+player.speed(0)
+
+# Set speed variable
+speed = 1
+
+# Define functions
+def turn_left():
+    player.left(30)
+
+def turn_right():
+    player.right(30)
+
+def increase_speed():
+    global speed
+    speed += 1
+
+# Set keyboard binding
+turtle.listen()
+turtle.onkey(turn_left, 'Left')
+turtle.onkey(turn_right, 'Right')
+turtle.onkey(increase_speed, 'Up')
+while True:
+    player.forward(speed)
